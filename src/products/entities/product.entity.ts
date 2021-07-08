d5810080f53f26be5bd063c6cd6d4cfcb05dee59
Renato Pereira <renato.laq@gmail.com>
2021-07-08 20:29:33 -0300

    Create entily products

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,30 @@
-export class Product {}
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  Entity,
+} from 'typeorm';
+// ORM Object Relation Mapper
+@Entity({ name: 'products' })
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  image_url: string;
+
+  @Column()
+  slug: string;
+
+  @Column()
+  price: number;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+}
